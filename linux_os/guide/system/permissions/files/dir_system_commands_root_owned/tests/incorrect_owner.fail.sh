d092d352ff6f0d452cb39732e87e67d09a6e71fe
rumch-se <77793453+rumch-se@users.noreply.github.com>
2021-05-03 17:01:20 +0200

    Update incorrect_owner.fail.sh

--- linux_os/guide/system/permissions/files/dir_system_commands_root_owned/tests/incorrect_owner.fail.sh
+++ linux_os/guide/system/permissions/files/dir_system_commands_root_owned/tests/incorrect_owner.fail.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
   
-#remediation none
-
 for TESTDIR in /bin/test_1 /sbin/test_1 /usr/bin/test_1 /usr/sbin/test_1 /usr/local/bin/test_1 /usr/local/sbin/test_1
 do
    if [[ ! -d $TESTDIR ]]
